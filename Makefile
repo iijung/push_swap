31855c593fc08b945ca30f83094471af47347f66
Minjung Kim <minjungk@c1r8s2.42seoul.kr>
2022-09-14 08:49:02 +0900

    init:

---
+++ Makefile
@@ -0,0 +1,70 @@
+# **************************************************************************** #
+#                                                                              #
+#                                                         :::      ::::::::    #
+#    Makefile                                           :+:      :+:    :+:    #
+#                                                     +:+ +:+         +:+      #
+#    By: minjungk <minjungk@student.42seoul.kr>     +#+  +:+       +#+         #
+#                                                 +#+#+#+#+#+   +#+            #
+#    Created: 2022/08/29 06:04:57 by minjungk          #+#    #+#              #
+#    Updated: 2022/09/14 08:42:13 by minjungk         ###   ########.fr        #
+#                                                                              #
+# **************************************************************************** #
+
+.DEFAULT_ON_ERROR:
+.DEFAULT_GOAL := all
+
+CC = cc
+AR = ar
+RM = rm -f 
+
+CFLAGS = -Wall -Wextra -Werror -MMD -MP
+CPPFLAGS = -I./libft
+LDFLAGS = -L./libft -lft
+
+ifdef DEBUG
+	CFLAGS += -O0 -g -fsanitize=address,undefined
+endif
+
+Q = @
+ifdef VERBOSE
+	Q =
+endif
+
+%.o : %.c
+	$(Q)$(COMPILE.c) $(OUTPUT_OPTION) $<
+
+# **************************************************************************** #
+# main
+# **************************************************************************** #
+NAME = push_swap
+
+src_m = \
+		push_swap.c
+
+src_b = \
+		push_swap.c
+
+SRCS = $(if $(filter bonus, $(MAKECMDGOALS)), $(src_b), $(src_m))
+OBJS = $(SRCS:.c=.o)
+DEPS = $(SRCS:.c=.d)
+LIBS = libft/libft.a
+
+all bonus:
+	$(Q)$(MAKE) -C $(dir $(LIBS)) $(notdir $(LIBS))
+	$(Q)$(MAKE) $(NAME)
+
+$(NAME): $(LIBS) $(OBJS)
+
+clean:
+	$(Q)$(MAKE) -C $(dir $(LIBS)) $(notdir $(LIBS)) clean
+	$(Q)$(RM) $(OBJS) $(DEPS)
+
+fclean: clean
+	$(Q)$(RM) $(LIBS) $(NAME)
+
+re: fclean
+	$(Q)$(MAKE) $(if $(filter bonus, $(MAKCMDGOALS)), bonus, all)
+
+.PHONY: all clean fclean re bonus $(LIBS)
+
+-include $(DEPS)
---
+++ libft/get_next_line.c
@@ -0,0 +1,145 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: minjungk <minjungk@student.42seoul.kr>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/08/21 05:46:00 by minjungk          #+#    #+#             */
+/*   Updated: 2022/08/29 08:57:57 by minjungk         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+static t_stream	*search_stream(t_stream **head, int fd)
+{
+	t_stream	*curr;
+	t_stream	*new;
+
+	if (head == 0)
+		return (0);
+	curr = *head;
+	while (curr)
+	{
+		if (curr->fd == fd)
+			return (curr);
+		if (curr->next == 0)
+			break ;
+		curr = curr->next;
+	}
+	new = ft_calloc(1, sizeof(t_stream));
+	if (new == 0)
+		return (0);
+	new->fd = fd;
+	if (curr)
+		curr->next = new;
+	else
+		*head = new;
+	return (new);
+}
+
+static void	clear_stream(t_stream **head, int fd)
+{
+	t_stream	*prev;
+	t_stream	*curr;
+	t_stream	*next;
+
+	if (head == 0)
+		return ;
+	prev = 0;
+	curr = *head;
+	while (curr)
+	{
+		if (curr->fd == fd)
+		{
+			next = curr->next;
+			free(curr);
+			if (prev)
+				prev->next = next;
+			else
+				*head = next;
+			return ;
+		}
+		prev = curr;
+		curr = curr->next;
+	}
+}
+
+static char	*join(char *dst, char *src, size_t slen, int *err)
+{
+	char	*tmp;
+	size_t	dlen;
+
+	if (src == 0 || src[0] == 0)
+		return (dst);
+	dlen = 0;
+	if (dst)
+		dlen = ft_strlen(dst);
+	if (slen > ft_strlen(src))
+		slen = ft_strlen(src);
+	tmp = (char *)ft_calloc(dlen + slen + 1, sizeof(char));
+	if (tmp == 0)
+	{
+		*err = 1;
+		free(dst);
+		return (0);
+	}
+	ft_memcpy(tmp, dst, dlen);
+	ft_memcpy(tmp + dlen, src, slen);
+	free(dst);
+	ft_memmove(src, src + slen, BUFFER_SIZE - slen);
+	ft_memset(src + BUFFER_SIZE - slen, 0, slen);
+	return (tmp);
+}
+
+static char	*get_line(t_stream *curr, int *close)
+{
+	ssize_t	idx;
+	char	*rtn;
+
+	if (curr == 0 || close == 0)
+		return (0);
+	rtn = 0;
+	idx = -1;
+	curr->read_len = BUFFER_SIZE;
+	while (1)
+	{
+		if (++idx == BUFFER_SIZE)
+		{
+			rtn = join(rtn, curr->read_buf, ft_strlen(curr->read_buf), close);
+			curr->read_len = read(curr->fd, curr->read_buf, BUFFER_SIZE);
+			if (curr->read_len < 0 || *close)
+				break ;
+			if (curr->read_len == 0)
+				*close = 2;
+			idx = 0;
+		}
+		if (idx == curr->read_len || curr->read_buf[idx] == '\n')
+			return (join(rtn, curr->read_buf, idx + 1, close));
+	}
+	free(rtn);
+	return (0);
+}
+
+char	*get_next_line(int fd)
+{
+	static t_stream	*stream;
+	t_stream		*curr;
+	char			*rtn;
+	int				close;
+
+	if (fd < 0)
+		return (0);
+	curr = search_stream(&stream, fd);
+	if (curr == 0)
+		return (0);
+	close = 0;
+	rtn = get_line(curr, &close);
+	if (rtn == 0 || close)
+		clear_stream(&stream, fd);
+	if (rtn && (close == 0 || close == 2))
+		return (rtn);
+	free(rtn);
+	return (0);
+}
