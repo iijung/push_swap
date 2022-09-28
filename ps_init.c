0caf7c49ad16cba46e4d8b3afce952a042b2b440
iijung <987kmj4@naver.com>
2022-09-28 19:18:42 +0900

    refactor:

--- ps_init.c
+++ ps_init.c
@@ -6,7 +6,7 @@
 /*   By: minjungk <minjungk@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/25 19:12:45 by minjungk          #+#    #+#             */
-/*   Updated: 2022/09/28 16:46:19 by minjungk         ###   ########.fr       */
+/*   Updated: 2022/09/28 19:17:39 by minjungk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -45,21 +45,14 @@ static int	swap(struct s_push_swap *ps, char cmd)
 {
 	if (ps == 0)
 		return (-1);
-	if (cmd == 's')
-	{
-		if (swap(ps, 'a') < 0 || swap(ps, 'b') < 0)
-			return (-1);
-	}
-	else if (cmd == 'a')
-	{
-		if (ps->a.swap(&ps->a) < 0)
-			return (-1);
-	}
-	else if (cmd == 'b')
-	{
-		if (ps->b.swap(&ps->b) < 0)
-			return (-1);
-	}
+	if (cmd == 'a')
+		return (ps->a.swap(&ps->a));
+	if (cmd == 'b')
+		return (ps->b.swap(&ps->b));
+	if (cmd != 's')
+		return (-1);
+	if (swap(ps, 'a') < 0 || swap(ps, 'b') < 0)
+		return (-1);
 	return (0);
 }
 
@@ -94,29 +87,15 @@ static int	command(struct s_push_swap *ps, char *cmd)
 	if (ps == 0 || cmd == 0)
 		return (-1);
 	if (!cmd[2] && cmd[0] == 'p' && ft_strchr("ab", cmd[1]))
-	{
-		if (push(ps, cmd[1]) < 0)
-			return (-1);
-	}
+		return (push(ps, cmd[1]));
 	else if (!cmd[2] && cmd[0] == 's' && ft_strchr("sab", cmd[1]))
-	{
-		if (swap(ps, cmd[1]) < 0)
-			return (-1);
-	}
+		return (swap(ps, cmd[1]));
 	else if (!cmd[2] && cmd[0] == 'r' && ft_strchr("rab", cmd[1]))
-	{
-		if (rotate(ps, cmd[1], 0) < 0)
-			return (-1);
-	}
+		return (rotate(ps, cmd[1], 0));
 	else if (!cmd[3] && !ft_memcmp(cmd, "rr", 2) && ft_strchr("rab", cmd[2]))
-	{
-		if (rotate(ps, cmd[2], 1) < 0)
-			return (-1);
-	}
+		return (rotate(ps, cmd[2], 1));
 	else
 		return (-1);
-	if (ps->show)
-		ps->show(ps);
 	return (0);
 }
 
