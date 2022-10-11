39f71230f8968305d955c5a1d9c84658f5034b63
iijung <987kmj4@naver.com>
2022-10-12 02:08:51 +0900

    fix: btoa sort3

--- ps_btoa.c
+++ ps_btoa.c
@@ -6,7 +6,7 @@
 /*   By: minjungk <minjungk@student.42seoul.>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/28 17:46:00 by minjungk          #+#    #+#             */
-/*   Updated: 2022/10/11 23:46:20 by minjungk         ###   ########.fr       */
+/*   Updated: 2022/10/12 02:08:08 by minjungk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,34 +16,29 @@ static int	under3(t_push_swap *ps)
 {
 	t_ps_value		val;
 
-	if (ps == 0)
-		ps_error();
 	ps_pivot(&ps->b, 3, &val);
+	if (ps->b.node[0]->next->rank == val.max)
+		ps->command(ps, "sb");
+	else if (ps->b.node[0]->next->next->rank == val.max && ps->b.size == 3)
+		ps->command(ps, "rrb");
 	if (ps->b.node[0]->rank == val.max)
 	{
 		ps->command(ps, "pa");
 		if (ps->b.node[0]->rank < ps->b.node[0]->next->rank)
 			ps->command(ps, "sb");
 		ps->command(ps, "pa");
+		return (ps->command(ps, "pa"));
 	}
-	else if (ps->b.node[0]->next->rank == val.max)
-		return (ps->command(ps, "sb") && under3(ps));
+	ps->command(ps, "rb") && ps->command(ps, "sb") && ps->command(ps, "pa");
+	if (ps->b.node[0]->rank < ps->b.node[1]->rank)
+		ps->command(ps, "rrb") && ps->command(ps, "pa");
 	else
-	{
-		ps->command(ps, "rb") && ps->command(ps, "sb") && ps->command(ps, "pa");
-		if (ps->b.node[0]->rank > ps->b.node[0]->next->rank)
-			ps->command(ps, "rrb") && ps->command(ps, "pa");
-		else
-			ps->command(ps, "pa") && ps->command(ps, "rrb");
-	}
-	ps->command(ps, "pa");
-	return (1);
+		ps->command(ps, "pa") && ps->command(ps, "rrb");
+	return (ps->command(ps, "pa"));
 }
 
-static int	check(t_push_swap *ps, unsigned int size, t_ps_value *val)
+static int	check(t_push_swap *ps, t_ps_value *val, unsigned int size)
 {
-	if (ps == 0 || val == 0)
-		ps_error();
 	if (size < 4)
 	{
 		if (size == 1)
@@ -74,9 +69,9 @@ void	ps_btoa(t_push_swap *ps, unsigned int size)
 
 	if (ps == 0)
 		ps_error();
-	if (check(ps, size, &val))
+	if (check(ps, &val, size))
 		return ;
-	while (val.rb + val.push < size)
+	while (val.rb + val.push < val.size)
 	{
 		if (ps->b.node[0]->rank <= val.pivot1)
 			val.rb += ps->command(ps, "rb");
@@ -90,5 +85,5 @@ void	ps_btoa(t_push_swap *ps, unsigned int size)
 	ps_atob(ps, val.push - val.ra);
 	ps_restore(ps, val.ra, val.rb);
 	ps_atob(ps, val.ra);
-	ps_btoa(ps, size - val.push);
+	ps_btoa(ps, val.size - val.push);
 }
